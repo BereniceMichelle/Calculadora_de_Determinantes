1003bbc0909ebc4415871a5daa4d1af61f0dd1b3
BereniceMichelle <70177969+BereniceMichelle@users.noreply.github.com>
2020-11-05 18:18:20 -0600

    Update numeros.php

--- numeros.php
+++ numeros.php
@@ -1,10 +1,14 @@
-<?php
+<html>
+	<body>
+		<?php
 
-			if(isset($_REQUEST["calcular"])){
+			
 
 				$n1=$_REQUEST["numero1"];
 				$n2=$_REQUEST["numero2"];
 
 				echo " $n1 $n2";
-		}
+		
 		?>
+	</body>
+	</html>
