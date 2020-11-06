37963978ee627682b52ba285e90a0661824aa548
BereniceMichelle <70177969+BereniceMichelle@users.noreply.github.com>
2020-11-05 18:13:12 -0600

    Update numeros.php

--- numeros.php
+++ numeros.php
@@ -2,9 +2,9 @@
 
 			if(isset($_REQUEST["calcular"])){
 
-				$n1=$_REQUEST["numero1"]
-				$n2=$_REQUEST["numero2"]
+				$n1=$_REQUEST["numero1"];
+				$n2=$_REQUEST["numero2"];
 
-				echo " $n1 $n2"
+				echo " $n1 $n2";
 		}
 		?>
